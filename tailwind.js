3ec04dacce57ecbeee36b2f495ebc671705975bb
Seba <sebapremium17@gmail.com>
2020-09-18 09:29:20 -0300

    Template Ready     Not recommended postcss Configuration

---
+++ tailwind.js
@@ -0,0 +1,13 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  target: 'relaxed',
+  prefix: '',
+  important: false,
+  separator: ':',
+  corePlugins: {},
+  plugins: [],
+};
